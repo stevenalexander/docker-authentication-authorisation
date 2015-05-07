4924ec0f1d6abd799d676e930c6e3a7d77152e07
Steven Alexander <steven.william.alexander@googlemail.com>
2015-05-07 14:09:49 +0100

    added Authorisation microservice

--- microservices/authorisation/src/main/java/com/example/authorisation/resources/AuthorisationResource.java
+++ microservices/authorisation/src/main/java/com/example/authorisation/resources/AuthorisationResource.java
@@ -1,14 +1,35 @@
 package com.example.authorisation.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Consumes({MediaType.APPLICATION_JSON})
-@Produces({MediaType.APPLICATION_JSON})
+@Path("/authorisations")
+@Produces({MediaType.TEXT_PLAIN})
 public class AuthorisationResource {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorisationResource.class);
+
     @GET
-    public String hasAuthorisation(){
-        throw new WebApplicationException(500);
+    @Path("/{permission}")
+    public Boolean hasAuthorisation(@HeaderParam("callerId") String callerId, @PathParam("permission") String permission){
+        LOGGER.info("checking if callerId: " + callerId + " has permission: " + permission);
+
+        Boolean callerHasPermission = false;
+
+        // in real application this is where persisted RBAC would queried
+        switch (permission) {
+            case "personView": callerHasPermission = true; break;
+            case "personEdit": callerHasPermission = callerId.equals("1") ? true : false; break; // Only admin can edit/add persons
+            case "personAdd":  callerHasPermission = callerId.equals("1") ? true : false; break;
+        }
+
+        if (callerHasPermission) {
+            return true;
+        }
+
+        throw new WebApplicationException(404);
     }
 }
